f6b47a2d762103382a6a1a9f137254f2479354f5
Ing. Jozef Sakalos <jsakalos@seznam.cz>
2011-04-29 15:12:10 +0200

    Switch to dynamic loading

--- app.js
+++ app.js
@@ -1,48 +1,21 @@
 
-// creates My.computer.NoteBook Class
-Ext.define('My.computer.NoteBook', {
-	 config: {
-		  hardware:'Dell'
-		 ,os:'Linux'
-		 ,price:500
-	 }
-	,constructor:function(config) {
-		this.initConfig(config);
-		return this;
+// enable and configure the loader
+Ext.Loader.setConfig({
+	 enabled:true
+	,paths:{
+		Ext:'../../src'
 	}
-	
-	,expensive:false
-
-	// called by setPrice
-	,applyPrice:function(price) {
-		price = price || 0;
-		price = parseFloat(price, 10);
-		this.expensive = price > 1000;
-		console.log(
-			  'Price = ' 
-			+ price 
-			+ ', expensive = ' 
-			+ this.expensive
-		);
-		return price;
-	}
-	
-	// called by setOs
-	,applyOs:function(os) {
-		if(!(/^(Linux|Mac|Win)/).test(os)) {
-			return 'Other'
-		}
-		return os;
-	}
-
 });
 
-// creates instance of My.computer.NoteBook Class
-var myComputer = Ext.create('My.computer.NoteBook', {
-	 hardware:'MacBook Pro'
-	,os:'Mac OS X'
-	,price:1800
-});
+// define our panel
+Ext.define('My.panel.Panel', {
+	 extend:'Ext.panel.Panel'
+	,config:{
+		 title:'My Panel'
+		,width:400
+		,height:300
+	}
+})
 
 // Inform user that we are OK and ready
 Ext.onReady(function(){
